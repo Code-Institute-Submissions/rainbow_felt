e001cdf75f9a7523a1bbe84d68e390d8be5c971d
Sean Thomas <futoisaru@gmail.com>
2017-12-04 20:50:19 +0000

    Added modal for enlarging images and improved messages

---
+++ static/js/products.js
@@ -0,0 +1,3 @@
+$('#viewed-product-image').on('click', function() {
+    $('#product-modal').modal('show');
+    });
---
+++ static/js/rainbow.js
@@ -0,0 +1,4 @@
+// Fade out and hide messages after 6 seconds
+$(document).ready(function(){
+    $(".js-message").delay(6000).fadeOut('slow');
+});
