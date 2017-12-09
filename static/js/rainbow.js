e6913efbd176aa54c31cf3126a7e6cdce3eaa882
Sean Thomas <futoisaru@gmail.com>
2017-12-09 11:27:01 +0000

    Fixed the responsiveness of some elements

--- static/js/rainbow.js
+++ static/js/rainbow.js
@@ -1,4 +1,13 @@
-// Fade out and hide messages after 6 seconds
+
 $(document).ready(function(){
+    // Set min height of main container on page load
+    $("#main-container").css('min-height', $(window).height() + 'px');
+    // Fade out and hide messages after 6 seconds
     $(".js-message").delay(6000).fadeOut('slow');
+});
+
+
+// On window resize change main container min-height
+$(window).resize(function(){
+    $("#main-container").css('min-height', $(window).height() + 'px');
 });
